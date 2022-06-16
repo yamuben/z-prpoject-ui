f641499887c059f6c175dd7a1a85ae73fdfac1ae
Ben <yamubbenjamin@gmail.com>
2022-06-16 14:01:33 +0200

    rebased

--- src/lib/apiAuth.js
+++ src/lib/apiAuth.js
@@ -5,6 +5,7 @@ import "dotenv/config";
 
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
+import {notification} from "antd";
 
 const resolve = (response) => {
   const { data } = response;
@@ -18,22 +19,17 @@ const reject = (error) => {
     store.remove("x-auth-token");
     return;
   }
-  // notification.error({
-  //   message: `Error`,
-  //   description: message,
-  // });
-  return (
-    <Alert severity="error">
-    <AlertTitle>Error</AlertTitle>
-    {message}
-  </Alert>
-  );
+  return notification.error({
+      message: `Error`,
+      description: message,
+    });
 };
 
 const instance = axios.create({
   // eslint-disable-next-line no-undef
-  // baseURL: "https://50.116.19.85:9090/vruum/auth/api/v1",
-  baseURL: "http://localhost:8080/api/v1/user",
+  baseURL: "https://z-project-irrembo.herokuapp.com/",
+
+  // baseURL: "http://localhost:8080/api/v1/user",
   headers: {
     "Content-Type": "application/json",
     "x-auth-token": `${store.get("x-auth-token")}`,
